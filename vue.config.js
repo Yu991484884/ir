930aca7541c2af7c3d5490704a25fb1596c9cae4
Yu991484884 <hokuohyuha@gmail.com>
2024-05-09 15:52:20 +0900

    Added netlify.toml and updated vue.config.js for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,23 @@
-const {defineConfig} = require('@vue/cli-service')
-module.exports = defineConfig({
-    transpileDependencies: true,
-    lintOnSave: false
-})
-// const { defineConfig } = require('@vue/cli-service');
-// const CopyPlugin = require("copy-webpack-plugin");
-
+// const {defineConfig} = require('@vue/cli-service')
 // module.exports = defineConfig({
-//   transpileDependencies: true,
-//   lintOnSave: false,
-//   configureWebpack: {
-//     plugins: [
-//       new CopyPlugin({
-//         patterns: [
-//           { from: "netlify.toml", to: "" }, // 从项目根目录复制到 dist/
-//         ],
-//       }),
-//     ],
-//   },
-// });
+//     transpileDependencies: true,
+//     lintOnSave: false
+// })
+
+
+const { defineConfig } = require('@vue/cli-service');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: "netlify.toml", to: "" }, // 从项目根目录复制到 dist/
+        ],
+      }),
+    ],
+  },
+});
